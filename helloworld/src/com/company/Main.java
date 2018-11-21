b7373a14573625211722c6ca728f51a72d3bd304
Sumit Dubey0005 <sumit.dubey0005@stud.hkr.se>
2018-11-21 14:52:49 +0100

    First version of my project.

---
+++ helloworld/src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int sum;
+
+        a=10;
+        b=20;
+        sum = a + b;
+
+        System.out.println(sum);
+
+    }
+}
